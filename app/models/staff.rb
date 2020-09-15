7e8715bca0f38015360381ff137a32d8ae76f989
Filonov Igor <filonovia@gmail.com>
2020-09-16 00:25:58 +0500

    add devise helpers in concern

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,10 +1,4 @@
 class Staff < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable,
-         :confirmable
+
+  include DeviseDefs
 end
