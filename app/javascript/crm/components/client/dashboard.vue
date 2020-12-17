38b063f076f71040504d431ba88857698bfd2422
Filonov Igor <filonovia@gmail.com>
2020-12-17 23:51:06 +0500

    add client companies filter

--- app/javascript/crm/components/client/dashboard.vue
+++ app/javascript/crm/components/client/dashboard.vue
@@ -7,10 +7,15 @@
             q-table(title="Companies" dense :data="client_companies" row-key="name"
               :pagination.sync="pagination"
               class="text-primary"
+              :filter="filter"
               :loading="loading")
               template(v-slot:loading)
                 q-inner-loading(showing)
                   q-spinner-dots(size="50px" color="primary")
+              template(v-slot:top-right)
+                q-input(borderless dense debounce="300" v-model="filter" placeholder="Search")
+                  template(v-slot:append)
+                    q-icon(name="fas fa-search")
 </template>
 
 <script>
@@ -23,7 +28,8 @@ export default {
       pagination: {
         rowsPerPage: process.env.COMPANIES_PER_PAGE // current rows per page being displayed
       },
-      loading: true
+      loading: true,
+      filter: ''
     }
   },
   methods: {
